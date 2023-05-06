63108521c8207a5e3f7421d372e724849ec6d655
Leandro Sacchi <59618736+leandrosacchi@users.noreply.github.com>
2023-05-06 17:49:09 -0300

    Add files via upload

---
+++ OlaUnicamp.java
@@ -0,0 +1,8 @@
+package br.unicamp.ic.inf335.app;
+
+public class OlaUnicamp {
+    public static void main(String[] args) {
+
+        System.out.println("Aplicacao executada com sucesso");
+    }
+}
